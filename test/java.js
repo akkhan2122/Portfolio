3e6a66a6abb4401e3f27a0d52a28a61ca0206769
Awais Khan <awaisak15869t72@gmail.com>
2024-06-17 12:54:30 +0500

    dc

---
+++ test/java.js
@@ -0,0 +1,120 @@
+
+// for (let i= 1; i <= 21; i++)
+// if(i  % 2 === 0){
+//     console.log(i + "even");
+// }
+// else{
+//     console.log(i  + "odd");
+// }
+//  {
+//     console.log(i);
+//    ;
+    
+// }
+
+
+// question 1 complete
+
+
+
+// question no 2
+
+
+// for (let i = 5; i <=100; i++) 
+// if(i%5 === 0){
+//     console.log(i);
+// }
+// {;
+    
+// }
+
+      // complete
+
+
+
+      // question 3
+
+    //   var num = prompt("enter your name");
+    //   var save = num[0].toLocaleUpperCase() + num.slice(1).toLocaleLowerCase();
+    //   console.log(save);
+      
+
+    //complete
+
+
+    //             practise start
+
+
+    // var prom1 = prompt("Enter a text");
+    // var arr =  [];
+    // for (let i = 0; i<prom1.length; i++){
+    //     console.log(arr [i]);
+    // }
+   
+
+    // var n = +prompt("Enter a text")
+    // var a  =[]
+    // for(let i = 0;  i <= n;  i++){
+    //     arr[i]= promt("enter a text")
+    // }
+
+    // var arr = [];
+    // var a = prompt("Enter a text")
+    // console.log(arr,a);
+
+
+
+    //                           practise  end
+ 
+// var big = [ 1 , 23 , 24 , 25 , 99 ];
+// var smallest = Math.min(...big);
+
+
+// var largest = Math.max(...big);
+// console.log("smallest number" , smallest);
+// console.log("largest number" , largest);
+
+    
+
+    
+      
+// var str = "abcd"
+// for(i=0; i<=str.length; i++){
+//   console.log(str [i]);
+// }
+
+
+// for(let i =1; i<=100; i++){
+//   if(i%2 === 0){
+//     console.log(i + " even");
+//   }else{
+//     console.log( i +" odd");
+//   }
+// }
+
+
+// for (let i =10; i>=1; i--){
+//   console.log(i);
+// }
+
+
+// var str = "thapa"
+// for( let i=5; i>=str.length; i--){
+//   console.log(str);
+// }
+
+// let arr = prompt("Enter your number")
+// console.log([arr]);
+
+
+// let arr = [];
+// let n = +prompt("Enter your number");
+
+// for(let i =0; i < n; i++){
+//   arr[i]= +prompt("enter your favourite number")
+// }
+// console.log(arr);
+
+// let str = "ahmed";
+// var save = str[0].toLocaleUpperCase() +str.slice(1).toLowerCase();
+// console.log(save);
